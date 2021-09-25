77104f85b42b84500129e0e798054faf757de41a
Enzo3322 <enzo.spag14@gmail.com>
2021-09-25 01:02:15 -0300

    finalizado

--- main.c
+++ main.c
@@ -9,166 +9,167 @@ char name[100];
 char modality[100];
 char originCountry[100];
 char teamName[100];
-
 // variables with global scope to access valueof content: cadastro de equipe
 char trainingLocal[100];
 char trainingDate[100];
 char competitionPlace[100];
 char outfit[100];
 char accommodationOfTeam[100];
-
-char temp;
-// ============================================================================//
-
-// TODO: Criar uma função que recebe 
+// ======================================================================= ===//
 
 int registerAthlete() {
   system("clear");
-  printf("╔══════════════════════════════════════════════════════════════════════╗ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                           CADASTRO DE ATLETA                         ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝ \n\n");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|              CADASTRO DE ATLETA               | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n\n");
 
   printf("Nome do Atleta: ");
-  scanf("%c", & temp);
-  fgets(name, 100, stdin);
+  scanf("%s", &name);
   printf("\n");
-
   printf("Modalidade: ");
-  scanf("%c", & temp);
-  fgets(modality, 100, stdin);
+  scanf("%s", & modality);
   printf("\n");
-
   printf("Pais de Origem: ");
-  scanf("%c", & temp);
-  fgets(originCountry, 100, stdin);
+  scanf("%s", & originCountry);
   printf("\n");
-
   printf("Nome da Equipe Olimipica do atleta: ");
-  scanf("%c", & temp);
-  fgets(teamName, 100, stdin);
+  scanf("%s", & teamName);
   printf("\n");
-
   printf("Atleta Cadastrado com Sucesso!\n\n");
   return 1;
 }
 
 int registerTeam() {
   system("clear");
-  printf("╔══════════════════════════════════════════════════════════════════════╗ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                           CADASTRO DE EQUIPE                         ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝ \n\n");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|               CADASTRO DE EQUIPE              | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n");
+  
   printf("\nLocal de Treinamento: ");
-  scanf("%c", & temp);
-  fgets(trainingLocal, 100, stdin);
+  scanf("%s", & trainingLocal);
   printf("\n");
-
   printf("Data do Treinamento: ");
-  scanf("%c", & temp);
-  fgets(trainingDate, 100, stdin);
+  scanf("%s", & trainingDate);
   printf("\n");
-
   printf("Local da Compenticao: ");
-  scanf("%c", & temp);
-  fgets(competitionPlace, 100, stdin);
+  scanf("%s", & competitionPlace);
   printf("\n");
-
   printf("Equipamentos Necessarios: ");
-  scanf("%c", & temp);
-  fgets(outfit, 100, stdin);
+  scanf("%s", & outfit);
   printf("\n");
-
   printf("Alojamento da Delegacao: ");
-  scanf("%c", & temp);
-  fgets(accommodationOfTeam, 100, stdin);
+  scanf("%s", & accommodationOfTeam);
   printf("\n");
-
   printf("Equipe Cadastrado com Sucesso!\n\n");
   return 1;
 }
 
 int listAthlete() {
-  printf("╔══════════════════════════════════════════════════════════════════════╗ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                     LISTA DE ATLETAS CADASTRADOS                     ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝ \n\n");
-  printf("%s", name);
-  printf("%s", modality);
-  printf("%s", originCountry);
-  printf("%s", teamName);
+   system("clear");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|         LISTA DE ATLETAS CADASTRADOS          | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n");
+  printf("|\tNOME\tMODALIDADE\tPAIS\tEQUIPE\t \n");
+  printf("|\t%s", name);
+  printf("\t%s", modality);
+  printf("\t\t%s", originCountry);
+  printf("\t%s", teamName);
+  printf("\n\n");
+  
+  float i;
+  printf("Digite 1 para continuar\n");
+  printf("Digite 2 para cadastrar um novo atleta\n");
+  scanf("%s", i);
 
+  if(i == 1){
+    return 1;
+  }else if(i == 2) {
+    return registerAthlete();
+  }else {
+    return 0;
+  }
   return 1;
 }
 
 int listTeam() {
-  printf("╔══════════════════════════════════════════════════════════════════════╗ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                      LISTA DE EQUIPES CADASTRADAS                    ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝ \n\n");
-  printf("%s", trainingLocal);
-  printf("%s", trainingDate);
-  printf("%s", competitionPlace);
-  printf("%s", outfit);
-  printf("%s", accommodationOfTeam);
-  return 1;
+  system("clear");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|          LISTA DE EQUIPES CADASTRADA          | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n");
+  printf("%s\n", trainingLocal);
+  printf("%s\n", trainingDate);
+  printf("%s\n", competitionPlace);
+  printf("%s\n", outfit);
+  printf("%s\n\n", accommodationOfTeam);
+
+  float i;
+  printf("\nDigite 1 para continuar\n");
+  printf("Digite 2 para cadastrar um novo atleta\n");
+  scanf("%s", i);
+  if(i == 1){
+    return 1;
+  }else if(i == 2) {
+    return registerAthlete();
+  }else {
+    return 0;
+  }
 }
 
 int menu() {
   system("clear");
-  printf("╔══════════════════════════════════════════════════════════════════════╗ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                     GESTÃO DE TAREFAS OLIMPÍADAS                     ║ \n");
-  printf("║                             MENU DE OPÇÕES                           ║ \n");
-  printf("╠══════════════════════════════════════════════════════════════════════╣ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                 1 - Cadastrar Atletas                                ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                 2 - Cadastrar Cadastrar Equipe                       ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                 3 - Exibir Atletas Cadastrados                       ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                 4 - Exibir Membros de Equipe Cadastrados             ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝ \n\n");
-
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|        SOFTWARE DE GESTAO OLIMPIADAS          | \n");
+  printf("|                MENU DE OPCOES                 | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|    1 - Cadastrar Atletas                      | \n");
+  printf("|                                               | \n");
+  printf("|    2 - Cadastrar Cadastrar Equipe             | \n");
+  printf("|                                               | \n");
+  printf("|    3 - Exibir Atletas Cadastrados             | \n");
+  printf("|                                               | \n");
+  printf("|    4 - Exibir Membros de Equipe Cadastrados   | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n");
   int r = 0;
   int selected;
-  scanf("%d", & selected);
+  scanf("%i", & selected);
   switch (selected) {
-  case 1:
-    r = registerAthlete();
+  case 1: r = registerAthlete();
     break;
-  case 2:
-    r = registerTeam();
+  case 2: r = registerTeam();
     break;
-  case 3:
-    r = listAthlete();
+  case 3: r = listAthlete();
     break;
-  case 4:
-    r = listTeam();
+  case 4: r = listTeam();
     break;
-  default:
-    r = 0;
-    printf("Selecione uma opção válida");
+  default: r = 0;
+    printf("Selecione uma opcao valida \n\n");
   }
    menu();
 }
 
-void handleLogin() {
-  printf("╔══════════════════════════════════════════════════════════════════════╗ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                                LOGIN                                 ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╠══════════════════════════════════════════════════════════════════════╣ \n");
-  printf("║                                                                      ║ \n");
-  printf("║                   INSEIR USUÁRIO E SENHA PARA LOGAR                  ║ \n");
-  printf("║                                                                      ║ \n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝ \n\n");
+int handleLogin() {
+  system("clear");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|                     LOGIN                     | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n");
+  printf("|                                               | \n");
+  printf("|      INSERIR USUARIO E SENHA PARA LOGAR       | \n");
+  printf("|                                               | \n");
+  printf("|-----------------------------------------------| \n\n");
 
   char log[15] = "admin";
   char log1[15];
@@ -182,15 +183,12 @@ void handleLogin() {
   if (strcmp(log, log1) == 0 && strcmp(pw, pw1) == 0) {
     menu();
   } else {
-    system("clear");
-    printf("\n                       DADOS INVALIDOS! TENTE NOVAMENTE\n\n");
+    printf("\n             DADOS INVALIDOS! TENTE NOVAMENTE\n\n");
     handleLogin();
 
   }
 }
 
 void main() {
-  system("clear");
   handleLogin();
-
 }
